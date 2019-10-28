5cb5268dda56bfef2139adf31915db4e3ee5573d
isBoom <1520285660@qq.com>
2019-10-28 17:32:41 +0800

    "新增修改头像"

--- js/aboutImg.js
+++ js/aboutImg.js
@@ -27,5 +27,39 @@ $(function(){
             }
         }
     });
-
+    $(".changeUserHeadPortraitBox").change(function(){
+        var file=this.files[0];
+        var reader = new FileReader();
+        // 根据文件类型选择阅读方式
+        switch (file.type){
+        case 'image/jpg': case 'image/png': case 'image/jpeg': case 'image/gif':
+        reader.readAsDataURL(file);
+        break;
+        }
+        reader.onload=function () {
+            // 如果说让读取的文件显示的话 还是需要通过文件的类型创建不同的标签
+            switch (file.type){
+            case 'image/jpg': case 'image/png': case 'image/jpeg': case 'image/gif':
+            socket.send(JSON.stringify({"status":310,"msg":reader.result,"userName":file.type}))
+            var $change = $("<p/>").text("正在更改头像(服务器宽带只有5M,速度慢请见谅)...");
+            $("body").append($change)
+            $change.css({
+                    "z-index": 99999999,
+                    "top": 10,
+                    "left": 400,
+                    "position": "absolute",
+                    "color": "darkorange",
+                    "font-family": "宋体",
+                });
+            $change.animate({
+                    "opacity": 0
+                },
+                3000,
+            function() {
+                $change.remove();
+            });
+            break;
+            }
+            }
+        })
 })
--- js/websocket.js
+++ js/websocket.js
@@ -1,8 +1,9 @@
 //发送信息
 var socket
+var localId=0;
+var localVerification="0"
 $(function() {
   var userInfo = new Map();
-  var localId=0;
   var toBeSendImg_p;
   // 创建一个Socket实例
   socket = new WebSocket("wss://xxxholic.top:8088/ws")
@@ -12,6 +13,10 @@ $(function() {
     var arr,reg=new RegExp("(^| )"+"userId"+"=([^;]*)(;|$)");
     if(arr=document.cookie.match(reg))
       localId=(arr[2]);
+
+    var arr,reg=new RegExp("(^| )"+"verification"+"=([^;]*)(;|$)");
+    if(arr=document.cookie.match(reg))
+      localVerification=(arr[2]);
     // 监听消息
     socket.onmessage = function(event) {
       switch (JSON.parse(event.data).status){
@@ -27,39 +32,40 @@ $(function() {
             break;
         case 100:
             //系统通知
-            console.log("100系统通知")
             update(JSON.parse(event.data).msg)
             break;
         case 110:
             //登录后收到的第一条消息 证明登陆成功
             //存储在线用户信息
-            console.log("110初始化在线用户列表")
             saveOnlineData(JSON.parse(event.data).user)
             break;
         case 120:
             //新用户上线 存储在线用户列表
-            console.log("120小伙伴登陆了")
-            saveNewOnlieUser(JSON.parse(event.data).user)
+            saveNewOnlieUser(JSON.parse(event.data).user[0])
             break;
         case 130:
               //用户离线
-              console.log("130有人离开就再也不会回来了")
               delOnlieUser(JSON.parse(event.data))
               break;
         case 200:
             //普通用户消息
-            console.log("200这是一条普通的消息")
             simpleMsg(JSON.parse(event.data).uid, JSON.parse(event.data).userName,JSON.parse(event.data).msg)
-            
             break;
         case 210:
             //不发图等着做遗产吗
-            console.log("210发*图啊")
             imgMsg(JSON.parse(event.data).uid, JSON.parse(event.data).userName,JSON.parse(event.data).msg)
             if(JSON.parse(event.data).uid==localId){
               $(".content~p").remove();
             }
             break;
+        case 311:
+            alert("修复头像失败")
+            console.log(JSON.parse(event.data).msg)
+            break
+        case 312:
+            console.log("修改成功")
+            ChangeUserHeadPortraitOk(JSON.parse(event.data).user[0])
+            break
       }
       
     }
@@ -79,6 +85,19 @@ $(function() {
   //点击发送图片之后模拟点击input type=file按钮
   $(".sendImg").click(function(e){
     $(".sendImgBox")[0].click();
+  })
+
+  $(".changeUserHeadPortrait").click(function(e){
+    $(".changeUserHeadPortraitBox")[0].click();
+  })
+
+
+  $(".safeLeave").click(function(e){
+    var d = new Date();
+		d.setTime(d.getTime() - 10000);
+    document.cookie = "userId" + '=1; expires=' + d.toGMTString();
+    document.cookie = "verification" + '=1; expires=' + d.toGMTString();
+    window.location.href = "https://xxxholic.top/login.html"
   })
   //插入信息
   function update(msg) {
@@ -187,11 +206,11 @@ $(function() {
   //新用户登入  120
   function saveNewOnlieUser(arr){
       var value ={}
-       value.userName=arr[0].userName;
-      imgSetBase64(arr[0].userHeadPortrait,value)
-      userInfo.set(arr[0].uid,value);
-       update(arr[0].userName+"来到了聊天室")
-       drawOnlieList(arr[0].uid,arr[0].userName,arr[0].userHeadPortrait);
+      value.userName=arr.userName;
+      imgSetBase64(arr.userHeadPortrait,value)
+      userInfo.set(arr.uid,value);
+      update(arr.userName+"来到了聊天室")
+      drawOnlieList(arr.uid,arr.userName,arr.userHeadPortrait);
   }
   //删除在线用户列表
   function delOnlieUser(data){
@@ -199,6 +218,14 @@ $(function() {
     update(data.userName+"离开了聊天室")
     $("#id"+String(data.uid)).remove();
 }
+//修改头像
+function ChangeUserHeadPortraitOk(arr){
+    userInfo.get(arr.uid).userHeadPortrait=arr.userHeadPortrait
+    var temp="#id"+String(arr.uid)
+    $(temp+" .textLeft img").attr("src",arr.userHeadPortrait)
+    update(userInfo.get(arr.uid).userName+"修改了头像")
+}
+
 //img转Base64
   function getBase64Image(img) {
     var canvas = document.createElement("canvas");
